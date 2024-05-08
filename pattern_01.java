925dbf8d59dc7903623911ab52edbd4a4bbfa0bb
omkar4771 <omkardaswadkar123@gmail.com>
2024-05-08 22:26:28 +0530

    patern 1

---
+++ pattern_01.java
@@ -0,0 +1,24 @@
+package nested_for_loop;
+import java.util.*;
+public class pattern_01 
+{
+
+	public static void main(String[] args) 
+	{
+		int i,j,n;
+		System.out.println("Enter the number");
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		
+		for(i=1;i<=n;i++) //row
+		{
+			for(j=1;j<=i;j++)//coloum
+			{
+				System.out.print(" "+j);
+			}
+			System.out.println(" ");
+
+		}
+			}
+
+}
