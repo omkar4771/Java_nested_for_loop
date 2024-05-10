5708d0cf8e3cbc6b59458e1d99c6b57c25405c17
omkar4771 <omkardaswadkar123@gmail.com>
2024-05-10 23:57:11 +0530

    pattern 22

---
+++ pattern_22.java
@@ -0,0 +1,28 @@
+package nested_for_loop;
+
+import java.util.Scanner;
+
+public class pattern_22 
+{
+	public static void main(String[] args)
+	{
+		int i,j,k,n;
+		System.out.println("Enter the number...");
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		
+		for(i=1;i<=n;i++)
+		{
+			for(k=n;k>=i;k--)
+			{
+				System.out.print(" ");
+			}
+			for(j=1;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}
+	}
+
+}
