11ba88967379bc6f126c2145ee8365ddd7b8d692
omkar4771 <omkardaswadkar123@gmail.com>
2024-05-10 23:56:19 +0530

    pattern 19

---
+++ pattern_19.java
@@ -0,0 +1,27 @@
+package nested_for_loop;
+import java.util.*;
+public class pattern_19 
+{
+
+	public static void main(String[] args) 
+	{
+		int i,j,k,n;
+		System.out.println("Enter the number ");
+		Scanner sc= new Scanner(System.in);
+		n= sc.nextInt();
+		
+		for(i=1;i<=n;i++)
+		{
+			for(k=1;k<=i;k++)//space
+			{
+				System.out.print(" ");
+			}
+			for(j=n;j>=i;j--)
+			{
+				System.out.print("* ");
+			}
+			System.out.println(" ");
+		}
+	}
+
+}
